7092f231f3d73dccf57f1c130a25ec0c57936be9
judy <judy0824@gmail.com>
2019-07-01 20:46:40 +0800

    Subject:
    Description: 分頁導向及標題

--- routes/life.js
+++ routes/life.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('life/index', { title: 'Express' });
+  res.render('life/study');
 });
+
+router.get('/study', function(req, res, next) {
+  res.render('life/study');
+});
+
+router.get('/food', function(req, res, next) {
+  res.render('life/food');
+});
+
+router.get('/play', function(req, res, next) {
+  res.render('life/play');
+});
+
+router.get('/live', function(req, res, next) {
+  res.render('life/live');
+});
+
+
 
 module.exports = router;
