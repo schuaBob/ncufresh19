834b3b9daa2a46e84a3200473a36d3d44e072220
yww1327 <yww1327@gmail.com>
2019-06-30 21:28:10 -0800

    comingsoon done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/comingsoon', function (req, res, next) {
+  res.render('comingsoon/index', {
+    title: '新生知訊網',
+    user: req.user
+  });
+});
+
 module.exports = router;
