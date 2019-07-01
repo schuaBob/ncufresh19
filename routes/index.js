9aefdf55be9f7be2687d81e1d932ba7eddfe9267
schuaBob <schua1013@gmail.com>
2019-07-01 15:13:20 +0800

    change multer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination:'public/分頁資料夾',
+  filename(req, file, callback) {
+    cb(null,"檔名自己取");
+  }
+})
+var upload = multer({storage:storage});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
