c7122264260a0b3de23d9623ba56a1f509a81393
vanyar <vanyar0409@gmail.com>
2019-07-02 20:26:32 +0800

    各分頁樣式

--- routes/documents.js
+++ routes/documents.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('documents/index', { title: 'Express' });
 });
 
+router.get('/documents', function(req, res, next) {
+  res.render('documents/index', { title: 'Express' });
+});
+
+//大學生
+router.get('/undergraduate_must', function(req, res, next) {
+  res.render('documents/undergraduate_must',{ title: 'undergraduate' });
+});
+
+router.get('/undergraduate_fresh', function(req, res, next) {
+  res.render('documents/undergraduate_fresh',{ title: 'undergraduate' });
+});
+
+router.get('/undergraduate_study', function(req, res, next) {
+  res.render('documents/undergraduate_study',{ title: 'undergraduate' });
+});
+
+//研究生
+router.get('/graduate_must', function(req, res, next) {
+  res.render('documents/graduate_must',{ title: 'graduate' });
+});
+
+router.get('/graduate_fresh', function(req, res, next) {
+  res.render('documents/graduate_fresh',{ title: 'graduate' });
+});
+
+//共同事項
+router.get('/common', function(req, res, next) {
+  res.render('documents/common',{ title: 'common' });
+});
+
 module.exports = router;
