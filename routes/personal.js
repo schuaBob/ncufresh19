dc5b3cd7f7e780193c1da18a3d693e1695cee11d
yww1327 <yww1327@gmail.com>
2019-07-03 20:47:51 +0800

    Personal & Link 排版

--- routes/personal.js
+++ routes/personal.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('personal/index', { title: 'Express' });
+  res.render('personal/index', { title: '個人專區' });
 });
 
 module.exports = router;
