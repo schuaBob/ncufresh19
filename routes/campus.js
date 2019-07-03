8c62f55eb2f2b3fa479ac9aa6377ab15c1f8943e
chuan <charlie890414@gmail.com>
2019-07-03 20:39:30 +0800

    change image and fixed bug

--- routes/campus.js
+++ routes/campus.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('campus/index', {
+    title: '校園地圖'
+  });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('campus/index', { title: '校園地圖' });
+router.get('/indexmodal', function (req, res, next) {
+  request('https://ncufresh18.ncu.edu.tw/campus/indexmodal?id='+req.query.id, function (error, response, body) {
+    console.log('body:', JSON.parse(body)); 
+    res.json(JSON.parse(body));
+  });
 });
 
 module.exports = router;
