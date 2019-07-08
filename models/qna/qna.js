f41c5a2f336d529bdf3a439d9f9b98ccac6d94d7
kuzuha891 <kuzuha891@gmail.com>
2019-07-08 20:56:20 +0800

    連結後端、觸底加載、版面調整

---
+++ models/qna/qna.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var qna = mongoose.Schema({    
+    postID: Number,
+    authorID : { type: String, default: "test" },
+    category: String,
+    title : String,
+    qContent : String,
+    aContent: { type: String, default: "" }, 
+    count: { type: Number, default: 0 }, //點擊次數    
+    reviewed: { type: Boolean, default: false }, //審核與否
+});
+module.exports = mongoose.model('qna',qna);
