29a293242bb3d699d2f3ed98b21c70152cedf663
Lucy Chiu <gfpss95114@gmail.com>
2019-07-08 20:37:21 +0800

    路由修改中

--- routes/video.js
+++ routes/video.js
@@ -6,8 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('video/index', { title: '影音專區 | 新生知訊網' });
 });
 
+/** 中大生活 */
 router.get('/v_nculife', function(req, res, next) {
   res.render('video/v_nculife', { title: '影音專區 | 新生知訊網' });
+});
+
+/** 中大生活底下 */
+router.get('/v_nculife/:id', function(req, res, next) {
+  if(req.params.id === "v_eating") {
+    res.render('video/video_model', { title: '食 | 新生知訊網' });
+  } else if(req.params.id === "v_living"){
+    res.render('video/video_model', { title: '住 | 新生知訊網' });
+  }else if (req.params.id === "") {
+    res.render('video/v_nculife', { title: '影音專區 | 新生知訊網' });
+  } else {
+    res.status(404)
+    res.end();
+  }
 });
 
 router.get('/v_interview', function(req, res, next) {
@@ -22,4 +37,12 @@ router.get('/v_eating', function(req, res, next) {
   res.render('video/v_eating', { title: '影音專區 | 新生知訊網' });
 });
 
+// router.get('/v_nculife/:v_button', function(req, res, next) {
+//   if (req.params.v_button == eating) {
+//     res.render('video/video_model'); 
+//   }
+// });
+
 module.exports = router;
+
+//res.render('video/', {id: res.params.id}, { title: '影音專區 | 新生知訊網' });
