5a2da4f375eaa366f826160e614c32796ee40bfc
kuzuha891 <kuzuha891@gmail.com>
2019-07-09 20:37:49 +0800

    計數、改良函數

--- routes/qna.js
+++ routes/qna.js
@@ -70,7 +70,7 @@ router.post('/toPost',function(req,res){
   console.log(count);
   //判斷是否登入  
   new qnaDB({
-    postID:1,
+    postID:getPostID(),
     category:req.body.category,
     title:req.body.title,
     qContent:req.body.question,     
@@ -82,23 +82,64 @@ router.post('/toPost',function(req,res){
   });
   
 });
-router.post('/getData',function(req,res){
 
-  var _category;
-  if(req.body.category=='all'){
-    _category = "";
-  }else{
-    _category= req.body.category;
-  }   
+router.post('/getData',function(req,res){
+  var category1;
+  switch(req.body.category){
+    case "life":
+      category1="校園生活";
+      break;
+    case "course":
+      category1="課程相關";
+      break;
+    case "affair":
+      category1="學生事務";
+      break;
+    case "other":
+      category1="其他";
+      break;
+    default:
+      category1="";
+  }
   var rowCount = parseInt(req.body.rowCount); 
 
-  //從第幾列開始之後加載10個問題
-  qnaDB.find().limit(4).skip(rowCount).exec(function(err,result){
+  if(category1==""){
+    //從第幾列開始之後加載10個問題
+    qnaDB.find().limit(12).skip(rowCount).exec(function(err,result){
+      if(err){
+        return err;
+      }
+      // console.log(result);
+      res.send(result);
+    });
+  }else{
+    qnaDB.find({category:category1}).limit(12).skip(rowCount).exec(function(err,result){
+      if(err){
+        return err;
+      }
+      // console.log(result);
+      res.send(result);
+    });
+  }
+  
+});
+router.post("/getQuestion",function(req,res){
+  qnaDB.find({postID:req.body.postID}).exec(function(err,result){
     if(err){
       return err;
-    }
-    // console.log(result);
+    }    
+    qnaDB.update({postID:req.body.postID},{$inc:{count:1}},function(err,result){
+      
+    });    
     res.send(result);
-  });
+  });  
+  
 });
+
+function getPostID(){  
+  var date = new Date();   
+  return (date.getYear()-100)+""+(date.getMonth()+1)+date.getDate()+date.getHours()+date.getMinutes()+date.getSeconds()+date.getMilliseconds();
+}
+
+
 module.exports = router;
