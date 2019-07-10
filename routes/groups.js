920da09ed64edb8e775d1856b35c577e2675836b
lsaac <lsaac@lsaacde-MacBook-Pro.local>
2019-07-10 21:20:00 +0800

    modify

--- routes/groups.js
+++ routes/groups.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var department = require('../models/groups/department');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('groups/index', { title: 'Express' });
+    res.render('groups/index', { title: 'Express' });
 });
 router.get('/department', function(req, res, next) {
-  res.render('groups/g_department', { title: 'department' });
+    res.render('groups/g_department', { title: 'department' });
 });
 router.get('/association', function(req, res, next) {
-  res.render('groups/g_association', { title: 'association' });
+    res.render('groups/g_association', { title: 'association' });
 });
 router.get('/community', function(req, res, next) {
-  res.render('groups/g_community', { title: 'community' });
+    res.render('groups/g_community', { title: 'community' });
 });
 router.get('/others', function(req, res, next) {
-  res.render('groups/g_others', { title: 'others' });
+    res.render('groups/g_others', { title: 'others' });
 });
+
 
 
 module.exports = router;
