f745af148a0c06c823a75195fe88059f74815771
yww1327 <yww1327@gmail.com>
2019-07-16 20:42:49 +0800

    register post

--- routes/personal.js
+++ routes/personal.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var checkUser = require('./check-user');
 var fs = require('fs');
+var multer = require('multer');
 var gm = require('gm').subClass({imageMagick: true});
 
 var mongoose = require('mongoose');
@@ -13,11 +14,7 @@ var picname;
 
 router.get('/', /*checkUser.isLoggedIn,*/ function(req, res, next) {
 
-  /*res.locals.name = req.session.name;
-  res.locals.id = req.session.id;
-  res.locals.score_sum = req.session.score_sum;*/
-
-  /*fs.access("public/personal/profile-photo/" + req.user.id + ".png", fs.constants.R_OK, (err) => {
+  fs.access("public/personal/profile-photo/" + req.user.id + ".png", fs.constants.R_OK, (err) => {
     if(err) {
       picname = "default-profile.png";
     } else {
@@ -26,15 +23,32 @@ router.get('/', /*checkUser.isLoggedIn,*/ function(req, res, next) {
     Question.find({name: req.user.id}).exec(function(err, question) {
       if(err) {
         return next(err);
-      }*/
+      }
         res.render('personal/index', {
           title: '個人專區',
-          //question: question,
+          question: question,
           user: req.user,
           picname: picname
         });
-    /*});
-  });*/
+    });
+  });
 });
+
+var storage = multer.diskStorage({
+  destination: "../public/personal/profile-photo",
+  filename: function(req, file, cb) {
+    var fileName = req.user.id + ".png";
+    User.update({id: req.user.id}, {$set: {profile_pic: fileName}}, function(err, result) {
+      if(err) {
+        return next(err);
+      }
+    });
+    cb(null, fileName);
+  }
+});
+
+var upload = multer({storage: storage});
+
+
 
 module.exports = router;
