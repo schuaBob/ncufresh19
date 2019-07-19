d857e990409863c45b1943f0b26495ed816b00bf
kuzuha891 <kuzuha891@gmail.com>
2019-07-19 22:19:29 +0800

    手機版 in progress

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about/index', { title: 'Express' });
+  res.render('about/index', { title: 'Express',user:req.user });
 });
 
 module.exports = router;
