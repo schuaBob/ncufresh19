8066842d1a0f7764c274cba63c2f6c23b6e69e49
eugenechou <eugene668822@yahoo.com.tw>
2019-07-25 21:36:38 +0800

    indexPage3 done

--- public/index/index.js
+++ public/index/index.js
@@ -1,3 +1,5 @@
+var current_calender;
+
 $(document).ready(() => {
     $('#fullpage').fullpage({
         anchors: ['indexPage', 'newsPage', 'callenderPage'],
@@ -16,10 +18,17 @@ $(document).ready(() => {
         $.fn.fullpage.moveTo("indexPage");
     });
     
-    $(".day").each(function(){
-        $(this).css("left", (this.id-1) * 10 + "%");
+    $.ajax({
+        url:    "calender_get_data",
+        method: "POST",
+        data:   { id: "aug" },
+        error: function(err){
+            alert("Some error occur...");
+        },
+        success: function(data){
+            append_circle(data);
+        }
     });
- 
 });
 
 $(".selectMonth").on("click", function(){
@@ -31,23 +40,29 @@ $(".selectMonth").on("click", function(){
             alert("Some error occur...");
         },
         success: function(data){
-            $("#days").empty();
-            var count = 0;
-            for(var i in data){
-                $("#days").append('<div class="day" id="' +  count + '"> <div class="dot"> <svg height="40" width="40"> <circle cx="20" cy="20" r="20" fill="#ec6d4f" /> </svg> </div> <div class="date">' + data[i].month + '/' + data[i].date + '</div> </div>');
-                count = count + 1;
-            }
-            adjust();
+            append_circle(data);
         }
     });
 });
 
-$(".day").on("click", function(){
-
-});
-
-function adjust(){
+function append_circle(data){
+    $("#days").empty();
+    var count = 0;
+    for(var i in data){
+        $("#days").append('<div class="day" id="' +  count + '"> <div class="dot"> <svg height="40" width="40"> <circle cx="20" cy="20" r="20" fill="#ec6d4f" /> </svg> </div> <div class="date">' + data[i].month + '/' + data[i].date + '</div> </div>');
+        count = count + 1;
+    }
     $(".day").each(function(){
         $(this).css("left", (this.id) * 10 + "%");
+    });
+    current_calender = data;
+    $(".day").on("click", function(){
+        $("#board-detail").empty();
+        var cnt = 0;
+        for(var i in current_calender) {
+            if(cnt == this.id)
+                $("#board-detail").append(current_calender[i].board_content);
+            cnt = cnt + 1;
+        }
     });
 }
