d02dddcf3037f3dca551151cf82c543566e3e687
schuaBob <schua1013@gmail.com>
2019-07-29 19:06:53 +0800

    save

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost:27017/ncufresh19', {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -43,16 +43,16 @@ app.use(minify());
 app.use(helmet());
 app.use(flash());
 app.use(session({
-    secret: 'ThisIsNcuFresh19Speaking.',
-    name: 'ncufresh.session.id',
-    resave: false,
-    /* 不要每次讀取就存回去一次 */
-    saveUninitialized: false,
-    /* 除非做儲存的動作，不然不要為每個使用者都存session */
-    store: new MongoStore({
-        mongooseConnection: mongoose.connection,
-        touchAfter: 24 * 3600 /* 沒動session的話，二十四小時之後再去動它 */
-    })
+  secret: 'ThisIsNcuFresh19Speaking.',
+  name: 'ncufresh.session.id',
+  resave: false,
+  /* 不要每次讀取就存回去一次 */
+  saveUninitialized: false,
+  /* 除非做儲存的動作，不然不要為每個使用者都存session */
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    touchAfter: 24 * 3600 /* 沒動session的話，二十四小時之後再去動它 */
+  })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -98,31 +98,22 @@ app.use('/personal', personal);
 // 常用連結
 app.use('/link', link);
 
-//auto update at 00:00 & 12:00
-// console.log("Initiallize auto schedule news 1...");
-// const cronNews1 = new cronJob(
-//     '00 00 00 * * *',
-//     ()=>{
-
-//     }
-// )
-
 //multer settings
 var multer = require('multer');
 var multerUpload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, `${__dirname}/public/imguploads`);
-        },
-        filename: (req, file, cb) => {
-            cb(null, file.originalname);
-        }
-    })
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, `${__dirname}/public/imguploads`);
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    }
+  })
 })
 
-app.post('/tinymceUploader', multerUpload.single('file'),(req, res, next) => {
+app.post('/tinymceUploader', multerUpload.single('file'), (req, res, next) => {
   console.log(req.file)
-  res.json({location:`/imguploads/${req.file.originalname}`})
+  res.json({ location: `/imguploads/${req.file.originalname}` })
 })
 
 //css and js
@@ -138,33 +129,33 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/css', express.static(__dirname + '/node_modules/fullpage.js/dist'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('index/error', { title: "新生知訊網", user: req.user });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('index/error', { title: "新生知訊網", user: req.user });
 });
 
 //------------------- added by 陳威捷 (用於ejs模板中的函式)
-app.locals.convertDateToString = function(date) {
-    var str = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
-    return str;
+app.locals.convertDateToString = function (date) {
+  var str = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+  return str;
 }
-app.locals.trunc = function(str) {
-        if (str.length > 15) {
-            return str.substr(0, 15) + "...";
-        } else {
-            return str;
-        }
-    }
-    //----------------------------------
+app.locals.trunc = function (str) {
+  if (str.length > 15) {
+    return str.substr(0, 15) + "...";
+  } else {
+    return str;
+  }
+}
+//----------------------------------
 
 module.exports = app;
