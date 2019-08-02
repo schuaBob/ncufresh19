2cf46f05d95732f12804ef1e10e2a19ca2b403aa
yww1327 <yww1327@gmail.com>
2019-08-02 22:01:33 +0800

    手機板

--- routes/personal.js
+++ routes/personal.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var checkUser = require('./check-user');
 var fs = require('fs');
 var multer = require('multer');
-var gm = require('gm').subClass({imageMagick: true});
+//var gm = require('gm').subClass({imageMagick: true});
+var sharp = require('sharp');
 
 var mongoose = require('mongoose');
 var User = require('../models/index/user');
-require('../models/qna/qna');
-var Question = mongoose.model('qna');
-
-var picname;
+var Question = require('../models/qna/qna');
 
 router.get('/', checkUser.isLoggedIn, function(req, res, next) {
+
+  var picname;
 
   fs.access("public/personal/profile-photo/" + req.user.id + ".png", fs.constants.R_OK, (err) => {
     if(err) {
@@ -51,43 +51,53 @@ var upload = multer({storage: storage});
 
 router.post('/editPicture', upload.single('picture'), function(req, res, next) {
   var fileName = "public/personal/profile-photo/" + req.user.id + ".png";
+  
   console.log(req.user.id + " upload picture");
   fs.access(fileName, fs.constants.R_OK, (err) => {
     if(err) {
       return next(err);
     } else {
-      gm(fileName).resize(200, 200, "!").write(fileName, function(err) {
+      sharp(fileName).resize(200, 200, {
+        fit: 'outside',
+        position: 'center'
+      }).toFile("public/personal/profile-photo/" + req.user.id + "-new.png", function(err) {
         if(err) {
-          return next(err);
+          return err;
         }
+        console.log("Resize");
+      });
+
+      fs.rename("public/personal/profile-photo/" + req.user.id + "-new.png", fileName, function(err) {
+        if(err) { return err; }
+        console.log("Rename");
         return;
-        res.redirect('/personal');
       });
     }
+
   });
   res.redirect('/personal');
 });
 
 router.get('/deleteQna/:id', checkUser.isLoggedIn, function(req, res, next) {
   if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.redirect('../');
+    return res.redirect('/');
   }
   Question.findById(req.params.id).exec(function(err, result) {
     if(err) {
       return next(err);
     }
     if(!result) {
-      res.redirect('../');
+      res.redirect('/');
     }
     if(result.authorID !== req.user.id) {
-      res.redirect('../');
+      res.redirect('/');
     }
     result.DeleteDate = Date.now();
     result.save(function(err) {
       if(err) {
         return next(err);
       }
-      res.redirect('../');
+      res.redirect('/');
     });
   });
 });
