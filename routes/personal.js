7afed1a7eb2edcf2181d2b756cef199e388f1325
schuaBob <schua1013@gmail.com>
2019-08-03 14:36:14 +0800

    ready to pull

--- routes/personal.js
+++ routes/personal.js
@@ -3,43 +3,53 @@ var router = express.Router();
 var checkUser = require('./check-user');
 var fs = require('fs');
 var multer = require('multer');
-var gm = require('gm').subClass({imageMagick: true});
+var gm = require('gm').subClass({
+  imageMagick: true
+});
 
 var mongoose = require('mongoose');
 var User = require('../models/index/user');
 require('../models/qna/qna');
 var Question = mongoose.model('qna');
 
-var picname;
 
-router.get('/', checkUser.isLoggedIn, function(req, res, next) {
 
+router.get('/', checkUser.isLoggedIn, function (req, res, next) {
+  var picname;
   fs.access("public/personal/profile-photo/" + req.user.id + ".png", fs.constants.R_OK, (err) => {
-    if(err) {
+    if (err) {
       picname = "default-profile.png";
     } else {
       picname = req.user.id + ".png";
     }
-    Question.find({postID: req.user.id}).exec(function(err, question) {
-      if(err) {
+    Question.find({
+      postID: req.user.id
+    }).exec(function (err, question) {
+      if (err) {
         return next(err);
       }
-        res.render('personal/index', {
-          title: '新生知訊網 | 個人專區',
-          question: question,
-          user: req.user,
-          picname: picname
-        });
+      res.render('personal/index', {
+        title: '新生知訊網 | 個人專區',
+        question: question,
+        user: req.user,
+        picname: picname
+      });
     });
   });
 });
 
 var storage = multer.diskStorage({
   destination: "public/personal/profile-photo",
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     var fileName = req.user.id + ".png";
-    User.update({id: req.user.id}, {$set: {profile_pic: fileName}}, function(err, result) {
-      if(err) {
+    User.update({
+      id: req.user.id
+    }, {
+      $set: {
+        profile_pic: fileName
+      }
+    }, function (err, result) {
+      if (err) {
         return next(err);
       }
     });
@@ -47,17 +57,19 @@ var storage = multer.diskStorage({
   }
 });
 
-var upload = multer({storage: storage});
+var upload = multer({
+  storage: storage
+});
 
-router.post('/editPicture', upload.single('picture'), function(req, res, next) {
+router.post('/editPicture', upload.single('picture'), function (req, res, next) {
   var fileName = "public/personal/profile-photo/" + req.user.id + ".png";
   console.log(req.user.id + " upload picture");
   fs.access(fileName, fs.constants.R_OK, (err) => {
-    if(err) {
+    if (err) {
       return next(err);
     } else {
-      gm(fileName).resize(200, 200, "!").write(fileName, function(err) {
-        if(err) {
+      gm(fileName).resize(200, 200, "!").write(fileName, function (err) {
+        if (err) {
           return next(err);
         }
         return;
@@ -68,23 +80,23 @@ router.post('/editPicture', upload.single('picture'), function(req, res, next) {
   res.redirect('/personal');
 });
 
-router.get('/deleteQna/:id', checkUser.isLoggedIn, function(req, res, next) {
-  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+router.get('/deleteQna/:id', checkUser.isLoggedIn, function (req, res, next) {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.redirect('../');
   }
-  Question.findById(req.params.id).exec(function(err, result) {
-    if(err) {
+  Question.findById(req.params.id).exec(function (err, result) {
+    if (err) {
       return next(err);
     }
-    if(!result) {
+    if (!result) {
       res.redirect('../');
     }
-    if(result.authorID !== req.user.id) {
+    if (result.authorID !== req.user.id) {
       res.redirect('../');
     }
     result.DeleteDate = Date.now();
-    result.save(function(err) {
-      if(err) {
+    result.save(function (err) {
+      if (err) {
         return next(err);
       }
       res.redirect('../');
