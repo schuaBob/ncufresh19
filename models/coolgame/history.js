f008695cba1a65eb774414e7057c5cc667f2d0a1
kuzuha891 <kuzuha891@gmail.com>
2019-08-07 19:26:03 +0800

    conflict fixed

--- models/coolgame/history.js
+++ models/coolgame/history.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var history = mongoose.Schema({
     Id:String,
     Time:String,
+    Name:{ type: String, default: "預設"},
     Date:{ type: Date, default: new Date() },
     Score:String
 });
