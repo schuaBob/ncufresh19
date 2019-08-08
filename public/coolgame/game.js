6764f74155ba4ba05434a34de9945540e40c26fa
Coolshanlan <joey0201020123@gmail.com>
2019-08-09 01:15:50 +0800

    嚴重bug/question數量不會重製

--- public/coolgame/game.js
+++ public/coolgame/game.js
@@ -2392,6 +2392,9 @@ function labby_init() {
   sea.y = 0;
   labbybutton_container.visible = true;
   dolphin_container.visible=false;;
+  for (var j = 0; j < 3; j++) {
+    question_num[j] = question_list[j].length;
+  }
   open_fishmanwalk();
   open_fish_ad();
   open_fish();
